9529a1f40c2cede46c3e8bc85b5124ffcae02f20
olegm25 <olegm25@gmail.com>
2017-12-29 10:19:34 +0200

    adding logging

--- app/controllers/ApplicationController.scala
+++ app/controllers/ApplicationController.scala
@@ -1,14 +1,17 @@
 package controllers
 
 import model.{Value, ValueForm}
+import play.api.Logger
 import play.api.mvc._
 import scala.concurrent.Future
 import service.PersistanceService
 import scala.concurrent.ExecutionContext.Implicits.global
 
 class ApplicationController extends Controller {
+  val logger = Logger(this.getClass())
 
   def index = Action.async { implicit request =>
+    logger.info("Listing all values")
     PersistanceService.listAllValues map { values =>
       Ok(views.html.index(ValueForm.form, values))
     }
@@ -19,6 +22,7 @@ class ApplicationController extends Controller {
       // if any error in submitted data
       errorForm => Future.successful(Ok(views.html.index(errorForm, Seq.empty[Value]))),
       data => {
+        logger.info(s"Adding value: ${data.stringValue}")
         val newValue = Value(0, data.stringValue)
         PersistanceService.addValue(newValue).map(res =>
           Redirect(routes.ApplicationController.index())
@@ -27,6 +31,7 @@ class ApplicationController extends Controller {
   }
 
   def deleteValue(id: Long) = Action.async { implicit request =>
+    logger.info(s"Deleting value for $id")
     PersistanceService.deleteValue(id) map { res =>
       Redirect(routes.ApplicationController.index())
     }
